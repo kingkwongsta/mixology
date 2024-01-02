84e16654bcefecd5d2fb08a197d60ff0f6c2ec26
kwongster <bryant.j.kwong@gmail.com>
2024-01-01 20:43:05 -0800

    import liquorchoice component into page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,10 @@
 import Image from "next/image";
-import { useState } from "react";
+import LiquorChoice from "./components/LiquorChoice";
 
 export default function Home() {
-  const [liquorChoice, setLiquorChoice] = useState();
-
-  const liquorOptions = ["Vodka", "Whiskey", "Rum", "Gin", "Tequila", "Brandy"];
-
-  function handleLiquorChoice(liquor) {
-    setLiquorChoice(liquor);
-  }
-
-  function createLiquorButtons() {
-    return liquorOptions.map((spirit, index) => {
-      return (
-        <button
-          key={index}
-          className="p-2 m-5 border-solid border-2 border-sky-500 rounded-lg"
-        >
-          {spirit}
-        </button>
-      );
-    });
-  }
-
   return (
     <main>
-      <div className="bg-slate-500 text-lime-300">HELLO</div>
-      <div>
-        <h2>What liquor are you interested in using?</h2>
-        <div>{createLiquorButtons()}</div>
-      </div>
+      <LiquorChoice />
     </main>
   );
 }
