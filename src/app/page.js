773f750d39d2b652217f84e00f8934f3870762ca
kwongster <bryant.j.kwong@gmail.com>
2024-01-01 20:29:55 -0800

    remove stock css from next js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-slate-200">
       <div className="bg-slate-500 text-lime-300">HELLO</div>
     </main>
   );
