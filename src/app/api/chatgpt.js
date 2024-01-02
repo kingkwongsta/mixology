1711ceebb389c4024d3743a861d93db3d207d1b2
kwongster <bryant.j.kwong@gmail.com>
2024-01-01 22:42:18 -0800

    test api routes

--- src/app/api/chatgpt.js
+++ src/app/api/chatgpt.js
@@ -8,16 +8,22 @@ export default async function handler(req, res) {
       apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY, // Use environment variable
     });
 
-    const response = await openai.createCompletion({
-      model: "gpt-3.5-turbo", // Or any other preferred model
-      prompt: prompt,
-      temperature: 0.7, // Adjust as needed
-      max_tokens: 150, // Adjust as needed
-      n: 1,
-      stop: null,
+    // const response = await openai.createCompletion({
+    //   model: "gpt-3.5-turbo", // Or any other preferred model
+    //   prompt: prompt,
+    //   temperature: 0.7, // Adjust as needed
+    //   max_tokens: 150, // Adjust as needed
+    //   n: 1,
+    //   stop: null,
+    // });
+
+    const response = await openai.chat.completions.create({
+      messages: [{ role: "user", content: "Say this is a test" }],
+      model: "gpt-3.5-turbo",
     });
 
-    res.status(200).json({ response: response.data.choices[0].text });
+    // res.status(200).json({ response: response.data.choices[0].text });
+    console.log(response);
   } catch (error) {
     console.error(error);
     res
