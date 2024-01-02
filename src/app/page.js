0e4fb15cbb5c2ac3ede510b711f7bcbf9a7fd7a5
kwongster <bryant.j.kwong@gmail.com>
2024-01-01 20:36:10 -0800

    function creates buttons for popular liquors

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,21 @@
 import Image from "next/image";
 
 export default function Home() {
+  const liquor = ["Vodka", "Whiskey", "Rum", "Gin", "Tequila", "Brandy"];
+
+  function createLiquorButtons() {
+    return liquor.map((spirit, index) => {
+      return <button key={index}>{spirit}</button>;
+    });
+  }
+
   return (
-    <main className="bg-slate-200">
+    <main>
       <div className="bg-slate-500 text-lime-300">HELLO</div>
+      <div>
+        <h2>What liquor are you interested in using?</h2>
+        <div>{createLiquorButtons()}</div>
+      </div>
     </main>
   );
 }
