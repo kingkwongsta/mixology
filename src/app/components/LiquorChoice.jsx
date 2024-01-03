0942b2ea948640504d566e94a939356dad62f51f
kwongster <bryant.j.kwong@gmail.com>
2024-01-02 17:37:38 -0800

    begin to move liquorbutton function to it's own component

--- src/app/components/LiquorChoice.jsx
+++ src/app/components/LiquorChoice.jsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { useState } from "react";
+import LiquorButtons from "./LiquorButtons";
 
 export default function LiquorChoice() {
   const [liquorChoice, setLiquorChoice] = useState();
   const [recipe, setRecipe] = useState();
-  const liquorOptions = ["Vodka", "Whiskey", "Rum", "Gin", "Tequila", "Brandy"];
 
   const handleLiquorChoice = async (liquor) => {
     setLiquorChoice(liquor);
@@ -31,20 +31,6 @@ export default function LiquorChoice() {
       console.log(error.response);
     }
   };
-
-  function createLiquorButtons() {
-    return liquorOptions.map((liquor, index) => {
-      return (
-        <button
-          key={index}
-          className="p-2 m-5 border-solid border-2 border-sky-500 rounded-lg"
-          onClick={() => handleLiquorChoice(liquor)}
-        >
-          {liquor}
-        </button>
-      );
-    });
-  }
 
   function renderRecipe() {
     return Object.entries(recipe).map(([key, value], index) => {
@@ -75,7 +61,7 @@ export default function LiquorChoice() {
   return (
     <div>
       <h2>What liquor are you interested in using?</h2>
-      <div>{createLiquorButtons()}</div>
+      <LiquorButtons />
       <div className="">{recipe ? renderRecipe() : ""}</div>
       <button
         onClick={() => showState()}
