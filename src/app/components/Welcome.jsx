b9c3ee31b35da8e161e01d61736e43a42d2f20d1
kwongster <bryant.j.kwong@gmail.com>
2024-01-03 21:41:42 -0800

    styling - spacing and button colors and text style

--- src/app/components/Welcome.jsx
+++ src/app/components/Welcome.jsx
@@ -1,9 +1,11 @@
 export default function Welcome() {
   return (
-    <div>
-      <h1 className="text2xl">Crafted for Your Palate</h1>
-      <h2>Discover a world of exquisite cokctails</h2>
-      <button className="p-2 rounded-2xl border-2">Start Mixing</button>
+    <div className="text-center space-y-4 mt-10">
+      <h1 className="text-2xl">Crafted for Your Palate</h1>
+      <h2 className="text-xl">Discover a world of exquisite cokctails</h2>
+      <button className="p-2 rounded-2xl border-2 border-cyan-300">
+        Start Mixing
+      </button>
     </div>
   );
 }
