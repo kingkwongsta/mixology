8bc3760b6240760eb65565b80face9cb5f9d7ff6
kwongster <bryant.j.kwong@gmail.com>
2024-01-03 21:37:38 -0800

    base text of welcome screen

---
+++ src/app/components/Welcome.jsx
@@ -0,0 +1,9 @@
+export default function Welcome() {
+  return (
+    <div>
+      <h1 className="text2xl">Crafted for Your Palate</h1>
+      <h2>Discover a world of exquisite cokctails</h2>
+      <button className="p-2 rounded-2xl border-2">Start Mixing</button>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import LiquorChoice from "./components/LiquorChoice";
 import ChatGPT from "./components/ChatGPT";
+import Welcome from "./components/Welcome";
 
 export default function Home() {
   return (
     <main>
-      <LiquorChoice />
+      <Welcome />
+      {/* <LiquorChoice /> */}
       {/* <ChatGPT /> */}
     </main>
   );
