becb0741bcd3db61ced961a35b0cbd6508244769
kwongster <bryant.j.kwong@gmail.com>
2024-01-04 19:42:18 -0800

    setup the different flavor profiles in a variable

--- src/app/components/Preferences.jsx
+++ src/app/components/Preferences.jsx
@@ -7,6 +7,37 @@ export default function Preferences() {
     "Ready for a flavor adventure? Answer a few quick questions and we'll whip up a drink you'll love!",
     "Forget bland beverages! Unmask your true cocktail personality with our fun and fiery quiz.",
     "Can't decide on a cocktail? Let's unlock your taste buds with a personalized mixology journey!",
+    "Imagine sipping the perfect drink, tailored just for you. Take our quiz and make it a reality!",
+    "Ditch the guesswork, find your flavor bliss! Discover your signature cocktail in minutes.",
+    "Elevate your evenings with bespoke cocktails! Unleash your inner bartender with our taste profile quiz.",
+  ];
+
+  const flavorProfile = [
+    {
+      profile: "Sweetness",
+      description:
+        "Do you prefer your cocktails sweet, tart, or balanced? Popular sweeteners include simple syrup, liqueurs, fruit juices, and flavored syrups.",
+    },
+    {
+      profile: "Sourness",
+      description:
+        "Citrus fruits, fresh herbs, and some liqueurs add a delightful tartness. Consider how much citrusy punch you want.",
+    },
+    {
+      profile: "Bitterness",
+      description:
+        "Bitters, aperitifs, and dark-colored spirits like whiskey can add a complex bitterness. Some enjoy it, while others prefer to avoid it.",
+    },
+    {
+      profile: "Spice",
+      description:
+        "Ginger, chili peppers, and spices like cinnamon or nutmeg can add a unique kick. Decide if you want a fiery cocktail or a smooth sip.",
+    },
+    {
+      profile: "Fruitiness",
+      description:
+        "Fresh fruits, fruit juices, and infused spirits can add vibrant fruit flavors. Consider your favorite fruits or whether you prefer a tropical or citrusy twist.",
+    },
   ];
 
   useEffect(() => {
@@ -17,6 +48,7 @@ export default function Preferences() {
   return (
     <div>
       <h1>{introMessages[randomNum]}</h1>
+      <h1>Let's start with the flavor profile</h1>
     </div>
   );
 }
