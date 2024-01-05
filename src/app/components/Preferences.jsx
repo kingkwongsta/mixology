7a8bfa863e307ed35bb070b0209a6cfe5c77bdf1
kwongster <bryant.j.kwong@gmail.com>
2024-01-04 19:25:19 -0800

    create welcome messagess and utilize state to figure out random seslection

--- src/app/components/Preferences.jsx
+++ src/app/components/Preferences.jsx
@@ -1,7 +1,17 @@
+"use client";
+import { useState, useEffect } from "react";
+
 export default function Preferences() {
+  const [randomNum, setrandomNum] = useState(0);
+  const introMessages = [
+    "Ready for a flavor adventure? Answer a few quick questions and we'll whip up a drink you'll love!",
+    "Forget bland beverages! Unmask your true cocktail personality with our fun and fiery quiz.",
+    "Can't decide on a cocktail? Let's unlock your taste buds with a personalized mixology journey!",
+  ];
+
   return (
     <div>
-      <h1>Let's figure out your flavor profile</h1>
+      <h1>{introMessages}</h1>
     </div>
   );
 }
