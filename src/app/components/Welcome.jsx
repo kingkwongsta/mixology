a18206ffdbc97c4efb79af522a09c9d378556804
Kwongster <bryant.j.kwong@gmail.com>
2024-01-06 19:37:09 -0800

    type on welcome screen - cocktail

--- src/app/components/Welcome.jsx
+++ src/app/components/Welcome.jsx
@@ -6,7 +6,7 @@ export default function Welcome({ display }) {
     <div>
       <div className="text-center space-y-4 mt-10">
         <h1 className="text-2xl">Crafted for Your Palate</h1>
-        <h2 className="text-xl">Discover a world of exquisite cokctails</h2>
+        <h2 className="text-xl">Discover a world of exquisite cocktails</h2>
         <button
           onClick={display}
           className="p-2 rounded-2xl border-2 border-cyan-300"
