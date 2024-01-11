520b178301b69beb4b235cc8b9142de7ea1a58de
Kwongster <bryant.j.kwong@gmail.com>
2024-01-10 17:20:46 -0800

    create generic component that create a dropdown below a button

---
+++ src/app/components/ButtonListDropdown.jsx
@@ -0,0 +1,35 @@
+export default function ButtonListDropdown({
+  buttonContent,
+  selectedButton,
+  setSelectedButton,
+  isHovered,
+  onButtonClick,
+}) {
+  return (
+    <>
+      <div className="button-container min-h-[200px]">
+        {buttonContent.map((content, index) => (
+          <button
+            key={index}
+            className={`flavor-button p-2 m-5 border-solid border-2 border-sky-500 rounded-lg ${
+              index === selectedButton && "bg-sky-500"
+            }`}
+            onMouseEnter={() => setIsHovered(content.description)}
+            onMouseLeave={() => setIsHovered(false)}
+            onClick={(e) => {
+              onButtonClick(event.target.textContent);
+              setSelectedButton(index);
+            }}
+          >
+            {content.profile}
+          </button>
+        ))}
+        {isHovered && (
+          <div className="hover-description mx-5 my-2 max-w-[400px]">
+            {isHovered}
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
