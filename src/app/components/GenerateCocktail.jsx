63e955b87022f3dea28502850be07c0ed26d605d
Kwongster <bryant.j.kwong@gmail.com>
2024-01-11 16:58:51 -0800

    useMemo generate button names and remove dependency

--- src/app/components/GenerateCocktail.jsx
+++ src/app/components/GenerateCocktail.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 export default function GenerateCocktail({
   userFlavor,
@@ -11,36 +11,46 @@ export default function GenerateCocktail({
   const [randomNum, setRandomNum] = useState();
   const [selected, setSelected] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [generateButton, setGenerateButton] = useState();
 
-  const generateNames = [
-    "Shake It Up!",
-    "Pour Me Something Perfect",
-    "Surprise Me, Bartender!",
-    "Let's Get This Party Started",
-    "Mix Me a Masterpiece",
-    "What's My Cocktail Personality?",
-    "Reveal My Drink Destiny",
-    "Craft My Cocktail Lab Experiment",
-    "Spin the Cocktail Wheel of Fortune",
-    "Deal Me a Delicious Drink", // Emoji included
-    "Find My Flavor Match",
-    "Unlock My Ultimate Cocktail",
-    "Quench My Thirst for Something New",
-    "Nail My Next Drink",
-    "Celebrate with a Custom Cocktail",
-    "Mix My Magic Potion",
-    "Create My Cocktail Kingdom",
-    "Blast Off to Flavortown",
-    "Escape to Cocktail Island",
-    "Shake Your Tailfeather (and My Cocktail)",
-  ];
+  const generateNames = useMemo(() => {
+    [
+      "Shake It Up!",
+      "Pour Me Something Perfect",
+      "Surprise Me, Bartender!",
+      "Let's Get This Party Started",
+      "Mix Me a Masterpiece",
+      "What's My Cocktail Personality?",
+      "Reveal My Drink Destiny",
+      "Craft My Cocktail Lab Experiment",
+      "Spin the Cocktail Wheel of Fortune",
+      "Deal Me a Delicious Drink", // Emoji included
+      "Find My Flavor Match",
+      "Unlock My Ultimate Cocktail",
+      "Quench My Thirst for Something New",
+      "Nail My Next Drink",
+      "Celebrate with a Custom Cocktail",
+      "Mix My Magic Potion",
+      "Create My Cocktail Kingdom",
+      "Blast Off to Flavortown",
+      "Escape to Cocktail Island",
+      "Shake Your Tailfeather (and My Cocktail)",
+    ];
+  });
+
+  // useEffect(() => {
+  //   const calculateRandomIndex = () =>
+  //     Math.floor(Math.random() * generateNames.length);
+  //   const randomIndex = calculateRandomIndex();
+  //   setRandomNum(randomIndex);
+  // }, [generateNames, generateNames.length]);
 
   useEffect(() => {
     const calculateRandomIndex = () =>
       Math.floor(Math.random() * generateNames.length);
     const randomIndex = calculateRandomIndex();
     setRandomNum(randomIndex);
-  }, [generateNames, generateNames.length]);
+  }, []);
 
   const getCocktail = async () => {
     setIsLoading(true);
