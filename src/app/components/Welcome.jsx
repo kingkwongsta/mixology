18b458fe499c4923ee7e8287d861783591c33f75
Kwongster <bryant.j.kwong@gmail.com>
2024-01-12 21:57:28 -0800

    setup margins for welcome component

--- src/app/components/Welcome.jsx
+++ src/app/components/Welcome.jsx
@@ -3,12 +3,12 @@ import { Button } from "@/components/ui/button";
 export default function Welcome({ display }) {
   return (
     <div>
-      <div className="text-center space-y-4 mt-10">
+      <div className="text-center space-y-8 mt-10">
         <h1 className="text-2xl">Crafted for Your Palate</h1>
         <h2 className="text-xl">Discover a world of exquisite cocktails</h2>
-        <Button onClick={display} className="">
-          Start Mixing
-        </Button>
+        <div className="my-[200px] m-10">
+          <Button onClick={display}>Start Mixing</Button>
+        </div>
       </div>
     </div>
   );
