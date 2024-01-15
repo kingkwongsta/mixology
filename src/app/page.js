8b6b8f01ed3ad9002e38c34f573564ce3c9ac806
Kwongster <bryant.j.kwong@gmail.com>
2024-01-14 23:28:06 -0800

    remove flavor state from page component

--- src/app/page.js
+++ src/app/page.js
@@ -12,45 +12,38 @@ import userStore from "./../lib/userStore";
 
 export default function Home() {
   const [showWelcome, setshowWelcome] = useState(true);
-  const [userFlavor, setUserFlavor] = useState();
+  // const [userFlavor, setUserFlavor] = useState();
+  const { userFlavor } = userStore();
   const [userLiquor, setUserLiquor] = useState();
   const [userMood, setUserMood] = useState();
   const [recipe, setRecipe] = useState();
 
-  const { userTest, setUserTest } = userStore();
-  const handleChange = (test) => setUserTest(test);
-
   return (
-    <main className="m-5 space-y-8">
-      <h1>HELLO</h1>
-      <button onClick={() => handleChange("chocolate")}>Chocolate</button>
-      <button onClick={() => console.log(userTest)}>get state</button>
+    <main className="m-3">
+      {showWelcome ? (
+        <Welcome display={() => setshowWelcome(false)} />
+      ) : (
+        <div>
+          <FlavorChoice />
+          <LiquorChoice userFlavor={userFlavor} setUserLiquor={setUserLiquor} />
+          <MoodChoice userMood={userMood} setUserMood={setUserMood} />
+          <GenerateCocktail
+            userFlavor={userFlavor}
+            userLiquor={userLiquor}
+            userMood={userMood}
+            recipe={recipe}
+            setRecipe={setRecipe}
+          />
+        </div>
+      )}
+      {/*----- DEBUG STATE -----*/}
+      <button
+        onClick={() => console.log(userFlavor)}
+        className="p-2 m-5 border-solid border-2 border-sky-500 rounded-lg"
+      >
+        Debug: get user Flavor
+      </button>{" "}
+      */
     </main>
-    // <main className="m-3">
-    //   {showWelcome ? (
-    //     <Welcome display={() => setshowWelcome(false)} />
-    //   ) : (
-    //     <div>
-    //       <FlavorChoice setUserFlavor={setUserFlavor} />
-    //       <LiquorChoice userFlavor={userFlavor} setUserLiquor={setUserLiquor} />
-    //       <MoodChoice userMood={userMood} setUserMood={setUserMood} />
-    //       <GenerateCocktail
-    //         userFlavor={userFlavor}
-    //         userLiquor={userLiquor}
-    //         userMood={userMood}
-    //         recipe={recipe}
-    //         setRecipe={setRecipe}
-    //       />
-    //     </div>
-    //   )}
-    //   {/*----- DEBUG STATE -----*/}
-    //   {/* <button
-    //     onClick={() => console.log(recipe)}
-    //     className="p-2 m-5 border-solid border-2 border-sky-500 rounded-lg"
-    //   >
-    //     Debug: get recipe state
-    //   </button> */}
-    //   {/*----- END DEBUG STATE -----*/}
-    // </main>
   );
 }
