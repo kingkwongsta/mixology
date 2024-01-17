276334cfa741655702ba6c953fa01b897463954d
Kwongster <bryant.j.kwong@gmail.com>
2024-01-16 20:33:44 -0800

    fix parent jsx element to be one

--- src/app/layout/QuestionLayout.jsx
+++ src/app/layout/QuestionLayout.jsx
@@ -1,28 +1,31 @@
 import Image from "next/image";
 
-export default function QuestionLayout({ children }) {
-  const { imageSrc, imageAlt, imagePosition } = children.props;
+export default function RightImage({
+  children,
+  imageSrc,
+  imageAlt,
+  imagePosition,
+}) {
+  // Access props directly, not from children's props
+  const isImageOnRight = imagePosition === "right";
 
   return (
-      
-    {imagePosition == "right" ? (
+    <>
+      {isImageOnRight ? (
         <div className="flex flex-row">
-        <div className=" basis-3/5 min-h-[500px]">{children}</div>
-              <div className="basis-2/5 min-h-[500px]">
-                <Image className="rounded-xl my-8" src={imageSrc} alt={imageAlt} />
-              </div>
-            </div>
-      ) :
-    (
-      <div className="flex flex-row">
-      <div className=" basis-3/5 min-h-[500px]">{children}</div>
-            <div className="basis-2/5 min-h-[500px]">
-              <Image className="rounded-xl my-8" src={imageSrc} alt={imageAlt} />
-            </div>
+          <div className="basis-3/5 min-h-[500px]">{children}</div>
+          <div className="basis-2/5 min-h-[500px]">
+            <Image className="rounded-xl my-8" src={imageSrc} alt={imageAlt} />
           </div>
-    )}
-
-  )
+        </div>
+      ) : (
+        <div className="flex flex-row">
+          <div className="basis-3/5 min-h-[500px]">{children}</div>
+          <div className="basis-2/5 min-h-[500px]">
+            <Image className="rounded-xl my-8" src={imageSrc} alt={imageAlt} />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
-
-
