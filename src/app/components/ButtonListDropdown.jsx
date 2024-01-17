dfda95c630c36eface577e53887b9c40b58f4855
Kwongster <bryant.j.kwong@gmail.com>
2024-01-16 20:15:58 -0800

    spacing of buttons and text

--- src/app/components/ButtonListDropdown.jsx
+++ src/app/components/ButtonListDropdown.jsx
@@ -14,26 +14,29 @@ export default function ButtonListDropdown({
   return (
     <>
       <div className="flex flex-row">
-        <div>
-          {buttonContent.map((content, index) => (
-            <Button
-              key={index}
-              className={`m-5 max-w-[150px] ${
-                index === selectedButton && "bg-sky-500"
-              }`}
-              onMouseEnter={() => setIsHovered(content.description)}
-              onMouseLeave={() => setIsHovered(false)}
-              onClick={(e) => {
-                onButtonClick(event.target.textContent);
-                setSelectedButton(index);
-                setQuestionIndex(1);
-              }}
-            >
-              {content.profile}
-            </Button>
-          ))}
+        <div className="basis-2/5">
+          <div className="flex flex-col">
+            {buttonContent.map((content, index) => (
+              <Button
+                key={index}
+                className={`my-2 max-w-[140px] ${
+                  index === selectedButton && "bg-sky-500"
+                }`}
+                onMouseEnter={() => setIsHovered(content.description)}
+                onMouseLeave={() => setIsHovered(false)}
+                onClick={(e) => {
+                  onButtonClick(event.target.textContent);
+                  setSelectedButton(index);
+                  setQuestionIndex(1);
+                }}
+              >
+                {index}
+                {content.profile}
+              </Button>
+            ))}
+          </div>
         </div>
-        <div>
+        <div className="basis-3/5">
           {isHovered && (
             <div className="hover-description mx-5 my-2 max-w-[400px]">
               {isHovered}
