07c7f8796bdbfd9cd1af339d9eb81eef9347ba13
Kwongster <bryant.j.kwong@gmail.com>
2024-01-16 19:46:04 -0800

    create two section with border to see sizing

--- src/app/layout/RightImage.jsx
+++ src/app/layout/RightImage.jsx
@@ -1,8 +1,14 @@
-export default function RightImage() {
+import FlavorChoice from "../components/FlavorChoice";
+
+export default function RightImage({ content, image }) {
   return (
-    <div>
-      <div>Cool Guy</div>
-      <div>Nice Guy</div>
+    <div className="flex flex-row">
+      <div className="border-solid border-2 border-sky-500 basis-3/5 min-h-[600px]">
+        <FlavorChoice />
+      </div>
+      <div className="border-solid border-2 border-sky-500 basis-2/5 min-h-[600px]">
+        Nice Guy
+      </div>
     </div>
   );
 }
