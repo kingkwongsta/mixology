b84e3402438ab92d8c8762833e7757c8a73e81f4
Kwongster <bryant.j.kwong@gmail.com>
2024-01-17 16:30:13 -0800

    modify transition to be slower

--- src/lib/transition.js
+++ src/lib/transition.js
@@ -6,7 +6,7 @@ export default function Transition({ children }) {
     <motion.div
       initial={{ y: 20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
-      transition={{ ease: "easeInOut", duration: 0.75 }}
+      transition={{ ease: "easeInOut", duration: 1.75 }}
     >
       {children}
     </motion.div>
