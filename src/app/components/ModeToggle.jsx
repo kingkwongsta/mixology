16dc76593b17483f030b278ef59998b87a883de1
Kwongster <bryant.j.kwong@gmail.com>
2024-01-18 20:54:41 -0800

    create a home button with icon

--- src/app/components/ModeToggle.jsx
+++ src/app/components/ModeToggle.jsx
@@ -1,8 +1,9 @@
 "use client";
 
 import * as React from "react";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { MoonIcon, SunIcon, HomeIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
+import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 
@@ -11,18 +12,28 @@ export default function ModeToggle() {
   const isDarkMode = theme === "dark";
 
   return (
-    <Button
-      variant="outline"
-      size="icon"
-      onClick={() => setTheme(isDarkMode ? "light" : "dark")}
-      className={`${isDarkMode ? "dark:text-white" : ""}`}
-    >
-      {isDarkMode ? (
-        <SunIcon className="h-[1.2rem] w-[1.2rem]" />
-      ) : (
-        <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
-      )}
-      <span className="sr-only">Toggle theme</span>
-    </Button>
+    <>
+      <Button
+        variant="outline"
+        size="icon"
+        onClick={() => setTheme(isDarkMode ? "light" : "dark")}
+        className={`${isDarkMode ? "dark:text-white" : ""}`}
+      >
+        {isDarkMode ? (
+          <SunIcon className="h-[1.2rem] w-[1.2rem]" />
+        ) : (
+          <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
+        )}
+        <span className="sr-only">Toggle theme</span>
+      </Button>
+      <Button
+        variant="outline"
+        size="icon"
+        className={`${isDarkMode ? "dark:text-white" : ""}`}
+      >
+        <HomeIcon className="h-[1.2rem] w-[1.2rem]" />
+        <span className="sr-only">AWERAWER</span>
+      </Button>
+    </>
   );
 }
