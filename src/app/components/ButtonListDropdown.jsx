8371d763eaf36ae2b915e32aa29aef2a54807b62
Kwongster <bryant.j.kwong@gmail.com>
2024-01-18 20:48:03 -0800

    remove center align

--- src/app/components/ButtonListDropdown.jsx
+++ src/app/components/ButtonListDropdown.jsx
@@ -78,8 +78,8 @@ export default function ButtonListDropdown({
 
   return (
     <>
-      <div className="flex flex-row justify-center">
-        <div className="basis-2/5 flex justify-center">
+      <div className="flex flex-row m-8">
+        <div className="basis-2/5 flex">
           <div className="flex flex-col">
             {buttonContent.map((content, index) => (
               <Button
