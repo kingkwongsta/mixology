cdfe26d066c8bae6c1a857295dbf5e50a02fcbb4
Kwongster <bryant.j.kwong@gmail.com>
2024-01-22 15:21:32 -0800

    move home button code over to header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,10 +1,24 @@
 "use client";
-import Link from "next/link";
 import ModeToggle from "./ModeToggle";
+import userStore from "@/lib/userStore";
+import { Button } from "@/components/ui/button";
 
 export default function Header() {
+  const { questionIndex, setQuestionIndex, setDrinkRecipe } = userStore();
+
   return (
     <div className="flex flex-row justify-center">
+      <Button
+        variant="outline"
+        size="icon"
+        onClick={() => {
+          setQuestionIndex(-questionIndex);
+          setDrinkRecipe("");
+        }}
+        className={`${isDarkMode ? "dark:text-white" : ""}`}
+      >
+        <HomeIcon className="h-[1.2rem] w-[1.2rem]" />
+      </Button>
       <ModeToggle />
     </div>
   );
