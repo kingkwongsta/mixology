dda84b997ea1d1a2b1897b24f67ffeb50f8b37c9
Kwongster <bryant.j.kwong@gmail.com>
2024-01-24 17:23:07 -0800

    return to home when clicking home button

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,8 +2,10 @@
 import Image from "next/image";
 import HomeButton from "./buttons/HomeButton";
 import ModeToggle from "./buttons/ModeToggle";
+import userStore from "@/lib/userStore";
 
 export default function Header() {
+  const { questionIndex, setQuestionIndex } = userStore();
   return (
     // <div className="flex items-center justify-center mx-1 bg-gradient-to-r from-[#F2ADA7] to-[#9BF2F2] ">
     //   <div className="flex-grow ">
@@ -21,6 +23,7 @@ export default function Header() {
           alt="home"
           width={25}
           height={25}
+          onClick={() => setQuestionIndex(-questionIndex)}
         />
       </div>
     </div>
