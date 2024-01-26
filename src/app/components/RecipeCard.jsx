0d64ee2347eab35f9d41f9ffe898cec24b7de863
Kwongster <bryant.j.kwong@gmail.com>
2024-01-25 22:47:46 -0800

    update stytling

--- src/app/components/RecipeCard.jsx
+++ src/app/components/RecipeCard.jsx
@@ -11,15 +11,15 @@ import {
 export default function RecipeCard({ drinkRecipe }) {
   return (
     <div>
-      <h2 className="text-center text-3xl font-semibold text-[#F2ADA7] mt-10 mb-3">
+      <h2 className="text-center text-3xl font-semibold text-slate-900 mt-7 mb-6">
         {drinkRecipe.name}
       </h2>
 
-      <div className="flex flex-row space-x-10 items-center">
+      <div className="flex flex-row space-x-10">
         <div className="flex flex-col space-y-8">
-          <Card className="w-[500px] border-[#2E83F2] border-none shadow-xl bg-slate-100">
+          <Card className="w-[500px] border-[#2E83F2] border-none shadow-xl bg-slate-100 p-3">
             <CardHeader>
-              <CardTitle className="text-[#9BF2F2] ">Ingredients</CardTitle>
+              <CardTitle className="text-slate-900 ">Ingredients</CardTitle>
             </CardHeader>
             <CardContent>
               <ul className="text-slate-800">
@@ -34,12 +34,12 @@ export default function RecipeCard({ drinkRecipe }) {
             </CardContent>
           </Card>
 
-          <Card className="w-[500px] border-none shadow-xl bg-[#2E83F2]">
+          <Card className="w-[500px] border-none shadow-xl bg-slate-200 p-3">
             <CardHeader>
-              <CardTitle className="text-[#9BF2F2] ">Instructions</CardTitle>
+              <CardTitle className="text-slate-900 ">Instructions</CardTitle>
             </CardHeader>
             <CardContent>
-              <ol>
+              <ol className="text-slate-800">
                 {drinkRecipe.instructions
                   .split("\n")
                   .map((instruction, index) => (
