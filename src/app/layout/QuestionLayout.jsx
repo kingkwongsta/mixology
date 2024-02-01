a46f932a0c2cd3cc0d340080d0f1672e7b536d1a
Kwongster <bryant.j.kwong@gmail.com>
2024-01-31 23:10:27 -0800

    Refactor right image layout with rounded background

--- src/app/layout/QuestionLayout.jsx
+++ src/app/layout/QuestionLayout.jsx
@@ -8,7 +8,9 @@ export default function RightImage({ children }) {
     <div className="m-10">
       {isImageOnRight ? (
         <div className="flex flex-row space-x-5">
-          <div className="basis-3/5 min-h-[500px]">{children}</div>
+          <div className="basis-3/5 min-h-[500px] bg-white bg-opacity-20 rounded-xl">
+            {children}
+          </div>
           <div className="basis-2/5 min-h-[500px]">
             <Image
               className="rounded-xl my-8 opacity-70"
