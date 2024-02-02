31b8b591ee27dc88f2eaf77ec7f40cd0d931da13
Kwongster <bryant.j.kwong@gmail.com>
2024-02-01 19:49:22 -0800

    Refactor image layout styles in QuestionLayout.jsx

--- src/app/layout/QuestionLayout.jsx
+++ src/app/layout/QuestionLayout.jsx
@@ -7,13 +7,13 @@ export default function RightImage({ children }) {
   return (
     <div className="m-10">
       {isImageOnRight ? (
-        <div className="flex flex-row space-x-5">
+        <div className="flex flex-row space-x-6">
           <div className="basis-3/5 bg-white bg-opacity-20 rounded-xl">
             {children}
           </div>
-          <div className="basis-2/5 h-full">
+          <div className="basis-2/5">
             <Image
-              className="rounded-xl my-8 opacity-70 object-cover"
+              className="object-cover w-full h-full rounded-xl opacity-70"
               src={imageSrc}
               alt={imageAlt}
               width={1000}
@@ -23,9 +23,9 @@ export default function RightImage({ children }) {
         </div>
       ) : (
         <div className="flex flex-row space-x-5">
-          <div className="basis-2/5 min-h-[500px]">
+          <div className="basis-2/5 min-h-[500px] bg-contain">
             <Image
-              className="rounded-xl my-8 opacity-70 object-cover"
+              className="rounded-xl my-8 opacity-70 bg-contain"
               src={imageSrc}
               alt={imageAlt}
               width={500}
