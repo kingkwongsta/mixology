cee87840d8dfeafbe3a905f928c39effae0ddc6e
Kwongster <bryant.j.kwong@gmail.com>
2024-02-01 20:48:52 -0800

    Update QuestionCard component styles

--- src/app/components/QuestionCard.jsx
+++ src/app/components/QuestionCard.jsx
@@ -1,7 +1,9 @@
 export default function QuestionCard({ question, buttons }) {
   return (
     <div className="m-8">
-      <h1 className="my-10 text-2xl text-[#FFFFFF]">{question}</h1>
+      <h1 className="my-10 text-2xl font-semibold text-[#FFFFFF]">
+        {question}
+      </h1>
       {buttons}
     </div>
   );
