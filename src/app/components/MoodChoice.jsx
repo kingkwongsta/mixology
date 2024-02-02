2b3c94bef06ea3f1504a651c59b1ba46be0b7d16
Kwongster <bryant.j.kwong@gmail.com>
2024-02-01 20:41:21 -0800

    Refactor MoodChoice component to use QuestionCard component

--- src/app/components/MoodChoice.jsx
+++ src/app/components/MoodChoice.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import ButtonList from "./ButtonList";
 import userStore from "@/lib/userStore";
+import QuestionCard from "./QuestionCard";
 
 export default function MoodChoice() {
   const { setUserMood } = userStore();
@@ -19,18 +20,16 @@ export default function MoodChoice() {
 
   return (
     <>
-      <h2 className="text-2xl mb-10 mt-8">
-        A single sip takes people back to a cherished moment, sparking joy,
-        nostalgia, or bittersweet longing
-      </h2>
-      <h2 className="text-xl mb-7 text-[#F2ADA7]">
-        What kind of mood are you in?
-      </h2>
-      <ButtonList
-        buttonContent={moodSelections}
-        selectedButton={selectedButton}
-        setSelectedButton={setSelectedButton}
-        onButtonClick={setUserMood}
+      <QuestionCard
+        question="What kind of mood are you in?"
+        buttons={
+          <ButtonList
+            buttonContent={moodSelections}
+            selectedButton={selectedButton}
+            setSelectedButton={setSelectedButton}
+            onButtonClick={setUserMood}
+          />
+        }
       />
     </>
   );
