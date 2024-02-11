107508f5c772d4110a6ce1485d3ba93a2e66b5bb
Kwongster <bryant.j.kwong@gmail.com>
2024-02-11 09:02:38 -0800

    Update button and text styles in ButtonList, ButtonListDropdown, and Welcome components

--- src/app/components/Welcome.jsx
+++ src/app/components/Welcome.jsx
@@ -17,17 +17,17 @@ export default function Welcome({ display }) {
           width={1200}
           height={300}
         />
-        <div className="absolute inset-0 flex flex-col items-center justify-start mt-[100px] text-center]">
-          <div className="pt-4 px-8 rounded-xl backdrop-blur-xs hover:border-[#D9984A] border-2 border-slate-300 shadow-lg">
-            <h1 className="text-4xl text-[#ffffff] font-bold mb-7 drop-shadow-md tracking-wide">
+        <div className="absolute inset-0 flex flex-col items-center justify-start mt-[100px] text-center] max-sm:mt-[30px]">
+          <div className="pt-4 px-8 rounded-xl backdrop-blur-xs hover:border-[#D9984A] border-2 border-slate-300 shadow-lg max-sm:w-[350px] max-sm:text-center">
+            <h1 className="text-4xl text-[#ffffff] font-bold mb-7 drop-shadow-md tracking-wide max-sm:text-2xl">
               Crafted for Your Palate
             </h1>
-            <h2 className="text-xl text-center font-semibold drop-shadow-md">
+            <h2 className="text-xl text-center font-semibold drop-shadow-md max-sm:text-md">
               Discover a world of exquisite cocktails
             </h2>
             {/* <h1>{introMessages[randomNum]}</h1> */}
             <Button
-              className="bg-[#FF6600] m-[100px] min-w-[200px] shadow-md text-lg text-slate-700 drop-shadow-lg hover:bg-[#8C4130]"
+              className="bg-[#FF6600] m-[100px] min-w-[200px] shadow-md text-lg text-slate-700 drop-shadow-lg hover:bg-[#ffd299] max-sm:m-4"
               onClick={() => setQuestionIndex(1)}
             >
               <span className="text-white drop-shadow-md">Start Mixing</span>
