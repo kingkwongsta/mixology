9978ed356880f87234fe27af9655be72e237d5a0
Kwongster <bryant.j.kwong@gmail.com>
2024-02-14 15:59:17 -0800

    Refactor createCompletion function to handle missing user preferences

--- src/app/actions.js
+++ src/app/actions.js
@@ -3,14 +3,12 @@ import OpenAI from "openai";
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-export async function createCompletion(prompt) {
-  if (!prompt) {
-    return { error: "prompt is required" };
+export async function createCompletion(userFlavor, userLiquor, userMood) {
+  if (!userFlavor && !userLiquor && !userMood) {
+    return { error: "preferences have not been set" };
   }
 
   // generate recipe using OpenAI - GPT-3.5
-  const { userFlavor, userLiquor, userMood } = prompt;
-
   const userPreferences = `contains ${userLiquor} and emphasizes a ${userFlavor} flavor profile for a ${userMood} mood`;
 
   const instructions = `create a unique cocktail based on the user preferences in the text delimited by triple periods, ensure the drink name doesn't use the same/similar words to ${userMood},${userFlavor},${userLiquor}, `;
--- src/app/components/GenerateCocktail.jsx
+++ src/app/components/GenerateCocktail.jsx
@@ -67,52 +67,10 @@ export default function GenerateCocktail({}) {
   async function action() {
     // setIsLoading(true);
 
-    const { error } = await createCompletion({
-      userFlavor,
-      userLiquor,
-      userMood,
-    });
+    const { error } = await createCompletion(userFlavor, userLiquor, userMood);
     if (error) {
       toast.error(error);
     }
-
-    // try {
-    //   const [gptResponse, imageResponse] = await Promise.all([
-    //     fetch("/api/gptrequest", {
-    //       method: "POST",
-    //       headers: { "Content-Type": "application/json" },
-    //       body: JSON.stringify({ userFlavor, userLiquor, userMood }),
-    //     }),
-    //     fetch("/api/image-gen", {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //       },
-    //       body: JSON.stringify({
-    //         prompt: userLiquor,
-    //       }),
-    //     }),
-    //   ]);
-
-    //   const gptData = await gptResponse.json();
-    //   const { images } = await imageResponse.json();
-    //   console.log(gptData.data.choices[0]);
-    //   if (gptData.data.choices && gptData.data.choices.length > 0) {
-    //     setDrinkRecipe(JSON.parse(gptData.data.choices[0].message.content));
-    //   } else {
-    //     setDrinkRecipe("Error: Unexpected response structure");
-    //     toast.error("Error: Unexpected response structure");
-    //   }
-
-    //   const imageURL = `data:image/jpeg;base64,${images[0].imageData}`;
-    //   setDrinkImage({ imageURL });
-    //   setIsLoading(false);
-    // } catch (error) {
-    //   console.error("Error:", error);
-    //   toast.error("Error:", error);
-    //   setDrinkRecipe("Error: Fetch failed");
-    //   setIsLoading(false);
-    // }
   }
   return (
     <div className="mt-10 flex flex-col items-center">
